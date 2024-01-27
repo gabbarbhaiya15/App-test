e0079d15d34cfd78c7e77e7d653a8bc5a2c00d9b
gabbarbhaiya15 <143590849+gabbarbhaiya15@users.noreply.github.com>
2024-01-28 01:33:54 +0530

    Update index.js

--- Gofire/index.js
+++ Gofire/index.js
@@ -68,11 +68,15 @@ app.use('/updateprofile', UpdateProfile);
 app.use('/friendpost', Friendspost);
 app.use('/search', search);
 
-app.use(express.static('Frontend/public'))
+if(process.env.NODE_ENV=="production"){
+app.use(express.static('Frontend/public'));
     
     app.get("*",(req,res)=>{
         res.sendFile(path.resolve(__dirname,'Frontend','public','index.html'))
     })
+
+}
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
